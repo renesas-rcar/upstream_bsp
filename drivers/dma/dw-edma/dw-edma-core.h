948c25c710c6eab69d865698937c51b77951348e
Serge Semin <Sergey.Semin@baikalelectronics.ru>
2023-01-13 20:14:00 +0300

    dmaengine: dw-edma: Use DMA engine device debugfs subdirectory
    Since all DW eDMA read and write channels are now installed in a framework
    of a single DMA engine device, move all the DW eDMA-specific debugfs nodes
    into a ready-to-use DMA-engine debugfs subdirectory. It's created during
    the DMA-device registration and can be found in the dma_device.dbg_dev_root
    field.
    
    Link: https://lore.kernel.org/r/20230113171409.30470-19-Sergey.Semin@baikalelectronics.ru
    Tested-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Serge Semin <Sergey.Semin@baikalelectronics.ru>
    Signed-off-by: Lorenzo Pieralisi <lpieralisi@kernel.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Acked-by: Vinod Koul <vkoul@kernel.org>
    (cherry picked from commit 6cb6e9c6050ac32c33466899b6488d5b6dd1d8e8)
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- drivers/dma/dw-edma/dw-edma-core.h
+++ drivers/dma/dw-edma/dw-edma-core.h
@@ -111,9 +111,6 @@ struct dw_edma {
 	raw_spinlock_t			lock;		/* Protect v0 shared registers */
 
 	struct dw_edma_chip             *chip;
-#ifdef CONFIG_DEBUG_FS
-	struct dentry			*debugfs;
-#endif /* CONFIG_DEBUG_FS */
 };
 
 struct dw_edma_sg {
