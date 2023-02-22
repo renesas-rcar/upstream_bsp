986f98fc3955eb772434d3526d2c222a71aa8967
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2023-02-22 09:27:23 +0900

    BSP rebase: add re-handled.sh
    To re-order handled file.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

---
+++ renesas-bsp/script/re-handled.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#===============================
+#
+# re-handled
+#
+# re-order "handled" file to be same order as "full" file.
+# It is easy to check the difference.
+# New file will be "handled2"
+#
+# 2023/02/21 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+#===============================
+
+# You need to use this script at ${LINUX}/renesas-bsp
+
+: > handles2
+for commit in `cut -d " " -f 1 full`
+do
+	grep ${commit} handled >> handled2
+done
