d5d9e1a6f85d40cda3a01047739dc35b8c077279
Geert Uytterhoeven <geert+renesas@glider.be>
2023-03-13 20:05:45 +0100

    BSP rebase: re-handled: Keep comments
    The "handled" file contains comments: one or more lines starting with a
    hash mark, preceding the line with the commit they apply to.
    
    Make sure to keep all comments when re-ordering the "handled" file to
    match the order of the "full" file.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- renesas-bsp/script/re-handled.sh
+++ renesas-bsp/script/re-handled.sh
@@ -12,8 +12,22 @@
 
 # You need to use this script at ${LINUX}/renesas-bsp
 
-: > handles2
 for commit in `cut -d " " -f 1 full`
 do
-	grep ${commit} handled >> handled2
-done
+	sed -n < handled \
+	"# Process all lines:
+	 # 1. In case of a comment: save comment
+	 #    H: Append pattern space to hold space,
+	 /^#/H;
+	 # 2. In case of a matching commit: print saved comments and commit
+	 #    H: Append pattern space to hold space,
+	 #    x: Exchange the contents of the hold and pattern spaces,
+	 #    p: Print the current pattern space,
+	 #    q: Quit.
+	 /^${commit}/{H; x; p; q};
+	 # 3. Any other commit: delete saved comments
+	 #    s: Clear pattern space (but it keeps the newline :-(,
+	 #    h: Copy pattern space to hold space.
+	 /^[0-9a-f]/ { s/.*//; h}"
+	# Empty lines are filtered by grep below
+done | grep -v "^$" > handled2
