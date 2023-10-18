159078986f903f6543dc57a600cc1998f4384620
Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
2023-10-18 17:56:24 +0900

    PCI: dwc: endpoint: Introduce .pre_init() and .deinit()
    Renesas R-Car Gen4 PCIe controllers require vendor-specific
    initialization before .init().
    
    To use dw->dbi and dw->num-lanes in the initialization code,
    introduce .pre_init() into struct dw_pcie_ep_ops. While at it,
    also introduce .deinit() to disable the controller by using
    vendor-specific de-initialization.
    
    Note that the ep_init in the struct dw_pcie_ep_ops should be
    renamed to init later.
    
    Link: https://lore.kernel.org/linux-pci/20231018085631.1121289-9-yoshihiro.shimoda.uh@renesas.com
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Reviewed-by: Serge Semin <fancer.lancer@gmail.com>
    (cherry picked from commit 9baa8a18e31b7167885c11c38841ce92bbe20f4f)
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- drivers/pci/controller/dwc/pcie-designware-ep.c
+++ drivers/pci/controller/dwc/pcie-designware-ep.c
@@ -646,6 +646,9 @@ void dw_pcie_ep_exit(struct dw_pcie_ep *ep)
 			      epc->mem->window.page_size);
 
 	pci_epc_mem_exit(epc);
+
+	if (ep->ops->deinit)
+		ep->ops->deinit(ep);
 }
 EXPORT_SYMBOL_GPL(dw_pcie_ep_exit);
 
@@ -754,6 +757,9 @@ int dw_pcie_ep_init(struct dw_pcie_ep *ep)
 	ep->phys_base = res->start;
 	ep->addr_size = resource_size(res);
 
+	if (ep->ops->pre_init)
+		ep->ops->pre_init(ep);
+
 	dw_pcie_version_detect(pci);
 
 	dw_pcie_iatu_detect(pci);
@@ -808,7 +814,7 @@ int dw_pcie_ep_init(struct dw_pcie_ep *ep)
 			       ep->page_size);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize address space\n");
-		return ret;
+		goto err_ep_deinit;
 	}
 
 	ep->msi_mem = pci_epc_mem_alloc_addr(epc, &ep->msi_mem_phys,
@@ -844,6 +850,10 @@ err_free_epc_mem:
 
 err_exit_epc_mem:
 	pci_epc_mem_exit(epc);
+
+err_ep_deinit:
+	if (ep->ops->deinit)
+		ep->ops->deinit(ep);
 
 	return ret;
 }
