3d0564fbfca88438e86a7b209a60e8dc56b2b4c8
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2024-02-13 10:33:29 +0900

    LTS: Renesas LTS for v6.12.x
    This branch is rebasing Renesas-LTS into v6.12.xx kernel
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

---
+++ renesas-lts/script/make-cve.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#===============================
+#
+# make-cve
+#
+# 1) You need to prepare latest cip-kernel-sec
+#	https://gitlab.com/cip-project/cip-kernel/cip-kernel-sec.git
+#
+# 2) checkout target branch
+#    it should be LTS stable kernel
+#                ^^^^^^^^^^^^^^^^^^^
+#
+# 3) goto cip-kernel-sec
+#
+# 4) call this script
+#
+# 2023/02/22 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+#===============================
+BSP=`readlink -f "$0" | xargs dirname | xargs dirname`
+V1=`head ../kernel/Makefile | grep "^VERSION"    | cut -d " " -f 3`
+V2=`head ../kernel/Makefile | grep "^PATCHLEVEL" | cut -d " " -f 3`
+V3=`head ../kernel/Makefile | grep "^SUBLEVEL"   | cut -d " " -f 3`
+
+TMP="/tmp/cve-$$"
+
+./scripts/report_affected.py stable/${V1}.${V2}:v${V1}.${V2}.${V3}	> ${TMP}-orig
+cat ${TMP}-orig | sed -e "s/ /\n/g" | sed 1d				> ${TMP}-affected
+(
+	cd issues
+	ls -1 CVE-*.yml | sed -e "s/\.yml$//g" 				> ${TMP}-all
+)
+cat ${TMP}-all ${TMP}-affected | sort -t"-" -k 2,2r -k 3nr | uniq -u	> ${TMP}-cve
+
+mv ${TMP}-cve ${BSP}/cve
+rm -f ${TMP}-*
