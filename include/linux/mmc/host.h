182f0ac6e90419201a7564d92605acf0f4ef7ee5
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2024-06-20 10:58:55 +0900

    Revert "mmc: core: mmc_ops: Enable stop command"
    This reverts commit 15f0be1cae313a46a473f71f3c43f7238ebcec1d.

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -421,7 +421,6 @@ struct mmc_host {
 #define MMC_CAP2_CRYPTO		0
 #endif
 #define MMC_CAP2_ALT_GPT_TEGRA	(1 << 28)	/* Host with eMMC that has GPT entry at a non-standard location */
-#define MMC_CAP2_STOP_TUNE_SD	(1 << 29)	/* Host need to send stop command during tuning in SD */
 
 	int			fixed_drv_type;	/* fixed driver type for non-removable media */
 
