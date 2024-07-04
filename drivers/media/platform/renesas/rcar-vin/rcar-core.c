65740b03602ba5dc389bf6c9c230ea0f1287eb36
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2024-07-04 18:16:18 +0200

    media: rcar-vin: Add family compatible for R-Car Gen4 family
    Add the Gen4 family compatible. This will be used instead of a SoC
    specific compatible for the new Gen4 SoC V4M. Two Gen4 boards (V3U and
    V4H) have already been added prior and their bindings need to be kept
    for backward compatibility.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Link: https://lore.kernel.org/r/20240704161620.1425409-5-niklas.soderlund+renesas@ragnatech.se
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    (cherry picked from commit 9a73e931308f89bfecf18787af29a21a92470fa8)

--- drivers/media/platform/renesas/rcar-vin/rcar-core.c
+++ drivers/media/platform/renesas/rcar-vin/rcar-core.c
@@ -1275,16 +1275,7 @@ static const struct rvin_info rcar_info_r8a77995 = {
 	.scaler = rvin_scaler_gen3,
 };
 
-static const struct rvin_info rcar_info_r8a779a0 = {
-	.model = RCAR_GEN3,
-	.use_mc = true,
-	.use_isp = true,
-	.nv12 = true,
-	.max_width = 4096,
-	.max_height = 4096,
-};
-
-static const struct rvin_info rcar_info_r8a779g0 = {
+static const struct rvin_info rcar_info_gen4 = {
 	.model = RCAR_GEN3,
 	.use_mc = true,
 	.use_isp = true,
@@ -1355,12 +1346,18 @@ static const struct of_device_id rvin_of_id_table[] = {
 		.data = &rcar_info_r8a77995,
 	},
 	{
+		/* Keep to be compatible with old DTS files. */
 		.compatible = "renesas,vin-r8a779a0",
-		.data = &rcar_info_r8a779a0,
+		.data = &rcar_info_gen4,
 	},
 	{
+		/* Keep to be compatible with old DTS files. */
 		.compatible = "renesas,vin-r8a779g0",
-		.data = &rcar_info_r8a779g0,
+		.data = &rcar_info_gen4,
+	},
+	{
+		.compatible = "renesas,rcar-gen4-vin",
+		.data = &rcar_info_gen4,
 	},
 	{ /* Sentinel */ },
 };
