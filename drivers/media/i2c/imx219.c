6df486d749c4bb6441a3898638a56a1efcab0988
Tarang Raval <tarang.raval@siliconsignals.io>
2025-03-06 11:04:44 +0530

    media: i2c: imx219: media: i2c: imx219: Enable runtime PM autosuspend
    Use pm_runtime_put_autosuspend() instead of pm_runtime_put()
    to allow autosuspend. Set a 1000ms autosuspend delay in
    imx219_probe() to improve power efficiency.
    
    Signed-off-by: Tarang Raval <tarang.raval@siliconsignals.io>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    (cherry picked from commit 5bd6b8c1bb2d49d5ff15c7c47d6b33d2d38785f0)

--- drivers/media/i2c/imx219.c
+++ drivers/media/i2c/imx219.c
@@ -772,7 +772,8 @@ static int imx219_enable_streams(struct v4l2_subdev *sd,
 	return 0;
 
 err_rpm_put:
-	pm_runtime_put(&client->dev);
+	pm_runtime_mark_last_busy(&client->dev);
+	pm_runtime_put_autosuspend(&client->dev);
 	return ret;
 }
 
@@ -793,7 +794,8 @@ static int imx219_disable_streams(struct v4l2_subdev *sd,
 	__v4l2_ctrl_grab(imx219->vflip, false);
 	__v4l2_ctrl_grab(imx219->hflip, false);
 
-	pm_runtime_put(&client->dev);
+	pm_runtime_mark_last_busy(&client->dev);
+	pm_runtime_put_autosuspend(&client->dev);
 
 	return ret;
 }
@@ -1280,6 +1282,8 @@ static int imx219_probe(struct i2c_client *client)
 	}
 
 	pm_runtime_idle(dev);
+	pm_runtime_set_autosuspend_delay(dev, 1000);
+	pm_runtime_use_autosuspend(dev);
 
 	return 0;
 
