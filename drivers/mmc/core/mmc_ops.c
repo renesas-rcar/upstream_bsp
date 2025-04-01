2444aa163217a16a6d56eba2250fa74bea2afd29
Wolfram Sang <wsa+renesas@sang-engineering.com>
2025-04-01 11:58:39 +0200

    mmc: rename mmc_can_ext_csd() to mmc_card_can_ext_csd()
    mmc_can_* functions sometimes relate to the card and sometimes to the host.
    Make it obvious by renaming this function to include 'card'. Also, convert to
    proper bool type while we are here.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Link: https://lore.kernel.org/r/20250401095847.29271-4-wsa+renesas@sang-engineering.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    (cherry picked from commit ceb5d3acb0c32beb3a8fbaf72f21e9eb1b58bd63)

--- drivers/mmc/core/mmc_ops.c
+++ drivers/mmc/core/mmc_ops.c
@@ -375,7 +375,7 @@ int mmc_get_ext_csd(struct mmc_card *card, u8 **new_ext_csd)
 	if (!card || !new_ext_csd)
 		return -EINVAL;
 
-	if (!mmc_can_ext_csd(card))
+	if (!mmc_card_can_ext_csd(card))
 		return -EOPNOTSUPP;
 
 	/*
@@ -936,7 +936,7 @@ out:
 	return err;
 }
 
-int mmc_can_ext_csd(struct mmc_card *card)
+bool mmc_card_can_ext_csd(struct mmc_card *card)
 {
 	return (card && card->csd.mmca_vsn > CSD_SPEC_VER_3);
 }
