fcb39266cac55ec7fd42d9b42be2fb78baed0060
Marek Vasut <marek.vasut+renesas@mailbox.org>
2026-04-06 17:52:54 +0200

    firmware: arm_scmi: Rename struct scmi_revision_info to scmi_base_info
    Rename struct scmi_revision_info to struct scmi_base_info , to
    accurately represent its content. The scmi_revision_info is no
    longer accurate, because the structure now contains more than
    only SCMI base protocol revision, it now also contains number
    of protocols, agents, vendor and subvendor strings. All those
    are fetched from the base protocol, so rename the structure to
    scmi_base_info, to match the other scmi_*_info structure names.
    No functional change.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
    Link: https://patch.msgid.link/20260406155343.72087-1-marek.vasut+renesas@mailbox.org
    Signed-off-by: Sudeep Holla <sudeep.holla@kernel.org>
    (cherry picked from commit 0c6eb5d019c1e4b9cfcfc47000e08858bbbc5e52)

--- drivers/firmware/arm_scmi/base.c
+++ drivers/firmware/arm_scmi/base.c
@@ -68,7 +68,7 @@ static int scmi_base_attributes_get(const struct scmi_protocol_handle *ph)
 	int ret;
 	struct scmi_xfer *t;
 	struct scmi_msg_resp_base_attributes *attr_info;
-	struct scmi_revision_info *rev = ph->get_priv(ph);
+	struct scmi_base_info *rev = ph->get_priv(ph);
 
 	ret = ph->xops->xfer_get_init(ph, PROTOCOL_ATTRIBUTES,
 				      0, sizeof(*attr_info), &t);
@@ -102,7 +102,7 @@ scmi_base_vendor_id_get(const struct scmi_protocol_handle *ph, bool sub_vendor)
 	int ret, size;
 	char *vendor_id;
 	struct scmi_xfer *t;
-	struct scmi_revision_info *rev = ph->get_priv(ph);
+	struct scmi_base_info *rev = ph->get_priv(ph);
 
 	if (sub_vendor) {
 		cmd = BASE_DISCOVER_SUB_VENDOR;
@@ -142,7 +142,7 @@ scmi_base_implementation_version_get(const struct scmi_protocol_handle *ph)
 	int ret;
 	__le32 *impl_ver;
 	struct scmi_xfer *t;
-	struct scmi_revision_info *rev = ph->get_priv(ph);
+	struct scmi_base_info *rev = ph->get_priv(ph);
 
 	ret = ph->xops->xfer_get_init(ph, BASE_DISCOVER_IMPLEMENT_VERSION,
 				      0, sizeof(*impl_ver), &t);
@@ -179,7 +179,7 @@ scmi_base_implementation_list_get(const struct scmi_protocol_handle *ph,
 	__le32 *num_skip, *num_ret;
 	u32 tot_num_ret = 0, loop_num_ret;
 	struct device *dev = ph->dev;
-	struct scmi_revision_info *rev = ph->get_priv(ph);
+	struct scmi_base_info *rev = ph->get_priv(ph);
 
 	ret = ph->xops->xfer_get_init(ph, BASE_DISCOVER_LIST_PROTOCOLS,
 				      sizeof(*num_skip), 0, &t);
@@ -378,7 +378,7 @@ static int scmi_base_protocol_init(const struct scmi_protocol_handle *ph)
 	u32 version;
 	char name[SCMI_SHORT_NAME_MAX_SIZE];
 	struct device *dev = ph->dev;
-	struct scmi_revision_info *rev = scmi_revision_area_get(ph);
+	struct scmi_base_info *rev = scmi_revision_area_get(ph);
 
 	ret = ph->xops->version_get(ph, &version);
 	if (ret)
