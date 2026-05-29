32b7765101a45baba7ae10d320e185b88a44b229
Linh Phung <linh.phung.jy@renesas.com>
2026-05-29 16:45:21 +0700

    media: rcar-vin: Deffer the driver probe until the Reset controller is readly
    Avoid driver probe failure when Reset controller is not ready.
    
    Signed-off-by: Linh Phung <linh.phung.jy@renesas.com>

--- drivers/media/platform/renesas/rcar-isp/csisp.c
+++ drivers/media/platform/renesas/rcar-isp/csisp.c
@@ -850,7 +850,11 @@ static int risp_probe_resources(struct rcar_isp *isp,
 #elif !defined(CONFIG_VIDEO_RCAR_VIN_VDK)
 	isp->rstc = devm_reset_control_get_shared(&pdev->dev, NULL);
 
-	return PTR_ERR_OR_ZERO(isp->rstc);
+	if (IS_ERR(isp->rstc)) {
+		dev_err(isp->dev, "failed to get reset \n"
+			);
+		return -EPROBE_DEFER;
+	}
 #endif
 
 	return 0;
