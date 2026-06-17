3e1b68f71d33c6706ee979be0b257b433c4a9d88
Sebastian Ene <sebastianene@google.com>
2026-06-17 11:11:01 +0000

    ANDROID: KVM: arm64: Use the correct number of pages for the FF-A pool
    While we correctly reserve a number of pages from the carevout that are
    used for the ff-a we might consume more than we are allowed to when
    ffa-unmap-on-lend is used. Account for the extra pages as well when we
    compute the location of the ff-a pool.
    
    Bug: 524891173
    Bug: 458241298
    Reported-by: Liu Xujian <liuxujian@xiaomi.corp-partner.google.com>
    Change-Id: I8ab4ac0c27005eb6e94e78d6917a14c0dc88d943
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/include/asm/kvm_pkvm.h
+++ arch/arm64/include/asm/kvm_pkvm.h
@@ -422,6 +422,8 @@ static inline unsigned long pkvm_selftest_pages(void) { return 0; }
  */
 #define KVM_FFA_MAX_NR_CONSTITUENTS	4096
 
+DECLARE_STATIC_KEY_FALSE(kvm_ffa_unmap_on_lend);
+
 static inline unsigned long hyp_ffa_proxy_pages(void)
 {
 	size_t desc_max;
@@ -449,6 +451,9 @@ static inline unsigned long hyp_ffa_proxy_pages(void)
 
 	/* Plus a page each for the hypervisor's RX and TX mailboxes. */
 	num_pages = (2 * KVM_FFA_MBOX_NR_PAGES) + DIV_ROUND_UP(desc_max, PAGE_SIZE);
+
+	if (static_branch_unlikely(&kvm_ffa_unmap_on_lend))
+		num_pages += KVM_FFA_SPM_HANDLE_NR_PAGES;
 
 	return num_pages;
 }
