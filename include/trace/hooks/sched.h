f46125658f98cce3e3c61859768a26edc32894d4
Hu Xile <huxile@oppo.com>
2026-07-01 20:49:35 +0800

    ANDROID: vendor_hook: Add a new hook in the __set_cpus_allowed_ptr_locked function to save the operation for skipped cpumasks in the ext scheduler.
    Vendor hooks added: android_vh_set_cpus_allowed_ptr_locked
    
    When using the ext scheduler, we optimize scenario-based scheduling by extending cpumask. After entering the ext scheduler, our requirements necessitate
    modifying cpumask, but the original cpumask is backed up to ensure restoration upon exiting the ext scheduler. Furthermore, the cpumask set during ext
    scheduling is also saved via a hook. Currently, we've encountered an issue: in __set_cpus_allowed_ptr_locked, if the mask set is the same as p->cpus_mask,
    it returns prematurely, causing the hook implemented in __do_set_cpus_allowed to fail to record the mask setting.Therefore, we want to add a hook to record
    the cpumask setting.
    
    Bug: 530116630
    
    Change-Id: Ib44ea5c498b5880b3dae2f71929909aee63865dd
    Signed-off-by: Hu Xile <huxile@oppo.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -434,6 +434,11 @@ DECLARE_RESTRICTED_HOOK(android_rvh_set_task_comm,
 DECLARE_HOOK(android_vh_move_entity,
 	TP_PROTO(int *move, unsigned int flags),
 	TP_ARGS(move, flags));
+
+DECLARE_HOOK(android_vh_sca_migrate_same,
+	TP_PROTO(struct task_struct *p, struct affinity_context *ctx),
+	TP_ARGS(p, ctx));
+
 DECLARE_HOOK(android_vh_scx_restore_flags,
 	TP_PROTO(const struct sched_class *prev_class,
 		 const struct sched_class *next_class,
