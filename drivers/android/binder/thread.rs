c6e266483e9040638c30971b837eb22d741029b1
Alice Ryhl <aliceryhl@google.com>
2026-07-07 07:18:20 +0000

    ANDROID: rust_binder: restore priority earlier in get_work()
    Once the waiting thread is added to the list of ready threads, remote
    threads can start boosting its priority. If the remote thread boosts the
    target thread priority before restore_priority() is called, then the
    thread ends up with the wrong priority while processing the transaction.
    
    Thus, restore the priority before adding the waiting thread to the list
    of ready threads.
    
    Bug: 525328558
    Change-Id: I06c922bfcbfee31ec29124631ca72c506f36f01b
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/thread.rs
+++ drivers/android/binder/thread.rs
@@ -620,6 +620,8 @@ impl Thread {
             }
         }
 
+        self.restore_priority(&self.process.default_priority);
+
         // If the caller doesn't want to wait, try to grab work from the process queue.
         //
         // We know nothing will have been queued directly to the thread queue because it is not in
@@ -639,8 +641,6 @@ impl Thread {
             if let Some(work) = inner.pop_work() {
                 return Ok(Some(work));
             }
-
-            self.restore_priority(&self.process.default_priority);
 
             inner.looper_flags |= LOOPER_WAITING | LOOPER_WAITING_PROC;
             let signal_pending = self.work_condvar.wait_interruptible_freezable(&mut inner);
