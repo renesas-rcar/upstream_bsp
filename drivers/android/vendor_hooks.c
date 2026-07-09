d561c2cb7c661c55089769e732cde21c8b6f1757
yongpeng li <yongpeng.li@transsion.com>
2026-07-09 17:01:13 +0800

    ANDROID: mmc: Add vendor hooks for sdcard failure diagnostics
    For sdcard failure diagnostics, add vendor hooks to monitor the sdcard
    identification and the sdcard reset failure in the request
    recovery handling.
    
    Link: https://android-review.googlesource.com/c/kernel/common/+/2070567
    
    It is a partial cherry-pick of android13-5.15 for some hooks, and removed
    the addition of struct members
    
    Bug: 533167747
    Change-Id: I4e7710e7b6eda4eed613c38f53b62ca8adab87f8
    Signed-off-by: yongpeng li <yongpeng.li@transsion.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -25,6 +25,7 @@
 #include <trace/hooks/preemptirq.h>
 #include <trace/hooks/fips140.h>
 #include <trace/hooks/ufshcd.h>
+#include <trace/hooks/mmc.h>
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/sys.h>
 #include <trace/hooks/iommu.h>
@@ -775,3 +776,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_override_exec_folio_order);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_swap_page_relax);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_do_swap_page_start);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_do_swap_page_done);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_blk_reset);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_attach_sd);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sdhci_get_cd);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mmc_gpio_cd_irqt);
