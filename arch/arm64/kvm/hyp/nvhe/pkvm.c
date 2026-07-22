thesofproject/linux

commit 36cf2dcf055eec9f014813bdb1605110a7679bf9

ANDROID: KVM: arm64: Disable devices at teardown start
At the moment we disable devices and free the domain in
__pkvm_finalize_teardown_vm() where the VM pages were already
reclaimed by the host, which might still have an elevated
refcount in the hyp.
Move the devices and domain teardown at the start of the
teardwon.

Bug: 508702141
Change-Id: Id48a905546ada5e96706804749cbdcf3d0325d2f
Signed-off-by: Mostafa Saleh <smostafa@google.com>

arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -1133,7 +1133,7 @@ int __pkvm_reclaim_dying_guest_page(pkvm_handle_t handle, u64 gfn, u64 nr_pages)
 
 	hyp_read_lock(&vm_table_lock);
 	hyp_vm = get_vm_by_handle(handle);
-	if (!hyp_vm || !hyp_vm->is_dying)
+	if (!hyp_vm || (hyp_vm->is_dying != PROTECTED_VM_DEAD))
 		goto unlock;
 
 	ret = __pkvm_host_reclaim_page_guest(gfn, nr_pages, hyp_vm);
@@ -1170,8 +1170,22 @@ int __pkvm_start_teardown_vm(pkvm_handle_t handle)
 		goto unlock;
 	}
 
-	hyp_vm->is_dying = true;
+	hyp_vm->is_dying = PROTECTED_VM_DYING;
 unlock:
+	hyp_write_unlock(&vm_table_lock);
+
+	/*
+	 * vCPUs are quiescent, but assigned devices may still be issuing DMA
+	 * via the guest's SMMU domains. Block DMA and tear down guest IOMMU
+	 * translations *before* allowing __pkvm_host_reclaim_page() to hand
+	 * OWNED pages (which may still carry a DMA hyp_page->refcount) back
+	 * to the host.
+	 */
+	pkvm_devices_teardown(hyp_vm);
+	pkvm_pviommu_teardown(hyp_vm);
+
+	hyp_write_lock(&vm_table_lock);
+	hyp_vm->is_dying = PROTECTED_VM_DEAD;
 	hyp_write_unlock(&vm_table_lock);
 
 	return ret;
@@ -1187,7 +1201,7 @@ int __pkvm_finalize_teardown_vm(pkvm_handle_t handle)
 
 	hyp_write_lock(&vm_table_lock);
 	hyp_vm = get_pkvm_unref_hyp_vm_locked(handle);
-	if (!hyp_vm || !hyp_vm->is_dying) {
+	if (!hyp_vm || (hyp_vm->is_dying != PROTECTED_VM_DEAD)) {
 		err = -EINVAL;
 		goto err_unlock;
 	}
@@ -1204,10 +1218,6 @@ int __pkvm_finalize_teardown_vm(pkvm_handle_t handle)
 		err = kvm_dying_guest_reclaim_ffa_resources(hyp_vm);
 	} while (err == -EAGAIN);
 	WARN_ON(err);
-
-	pkvm_devices_teardown(hyp_vm);
-
-	pkvm_pviommu_teardown(hyp_vm);
 
 	/*
 	 * At this point all page tables are destroyed and should be pushed to the pool

arch/arm64/kvm/hyp/include/nvhe/pkvm.h
@@ -56,6 +56,12 @@ struct kvm_ffa_buffers {
 	u64 vm_creating_bitmap;
 };
 
+enum protected_vm_state {
+	PROTECTED_VM_ALIVE = 0,
+	PROTECTED_VM_DYING,
+	PROTECTED_VM_DEAD,
+};
+
 /*
  * Holds the relevant data for running a vm in protected mode.
  */
@@ -89,11 +95,11 @@ struct pkvm_hyp_vm {
 	hyp_spinlock_t vcpus_lock;
 
 	/*
-	 * True when the guest is being torn down. When in this state, the
-	 * guest's vCPUs can't be loaded anymore, but its pages can be
+	 * Bigger than one when the guest is being torn down. When in this state,
+	 * the guest's vCPUs can't be loaded anymore, but its pages can be
 	 * reclaimed by the host.
 	 */
-	bool is_dying;
+	enum protected_vm_state is_dying;
 
 	struct kvm_ffa_buffers ffa_buf;
 	struct list_head vm_list;

renesas-rcar/upstream_bsp

commit 33c9aac27cf33a4ec6c72ae5f743e1fb116a0bbe

ANDROID: KVM: arm64: Do not teardown invalid VMs
If any problem found in the return of get_vm_by_handle() we
should immediately return.

Bug: 534604761
Fixes: 36cf2dcf055e ("ANDROID: KVM: arm64: Disable devices at teardown start")
Reported-by: Hiroyuki Katsura <hk590@cam.ac.uk>
Change-Id: Id9941f3172c4cacfb5eea844bbcb0a334bcc589f
Signed-off-by: Mostafa Saleh <smostafa@google.com>

arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -1171,7 +1171,6 @@ int __pkvm_start_teardown_vm(pkvm_handle_t handle)
 	}
 
 	hyp_vm->is_dying = PROTECTED_VM_DYING;
-unlock:
 	hyp_write_unlock(&vm_table_lock);
 
 	/*
@@ -1186,6 +1185,8 @@ unlock:
 
 	hyp_write_lock(&vm_table_lock);
 	hyp_vm->is_dying = PROTECTED_VM_DEAD;
+
+unlock:
 	hyp_write_unlock(&vm_table_lock);
 
 	return ret;