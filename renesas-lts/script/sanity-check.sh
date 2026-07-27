2cb57c6d1aff3c38e72f86d67827a5dd51638478
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2026-07-28 08:35:11 +0900

    LTS: tidyup renesas-lts files location
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- renesas-lts/script/sanity-check.sh
+++ renesas-lts/script/sanity-check.sh
@@ -10,13 +10,13 @@ TOP=`readlink -f "$0" | xargs dirname | xargs dirname`
 TMP=/tmp/sanity-check-$$
 
 ${TOP}/script/commit-ignored.sh			>  ${TMP}
-grep -vh '^#' ${TOP}/remains*			>> ${TMP}
-grep -v  '^#' ${TOP}/active			>> ${TMP}
-grep -v  '^#' ${TOP}/handled			>> ${TMP}
+grep -vh '^#' ${TOP}/commits/remains*		>> ${TMP}
+grep -v  '^#' ${TOP}/commits/active		>> ${TMP}
+grep -v  '^#' ${TOP}/commits/handled		>> ${TMP}
 
 cat ${TMP} | sort | uniq -d
 
-grep -v '^#' ${TOP}/full			>> ${TMP}
+grep -v '^#' ${TOP}/commits/full		>> ${TMP}
 
 cat ${TMP} | sort | uniq -u
 
