95d249092d60009d6c9ff5b299ea0c833ebd0990
Nobuhiro Iwamatsu <nobuhiro.iwamatsu.x90@mail.toshiba>
2026-09-04 21:55:48 +0900

    can: rcar_canfd: Invert reset assert order
    [ Upstream commit 41c13eaf39932fc79aa1ac245a9b97090fe23d5e ]
    
    The two resets are asserted during cleanup in the same order as they
    were deasserted during probe.  Invert the order to restore symmetry.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Vincent Mailhol <mailhol@kernel.org>
    Reviewed-by: Biju Das <biju.das.jz@bp.renesas.com>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Link: https://patch.msgid.link/20251124102837.106973-2-biju.das.jz@bp.renesas.com
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.x90@mail.toshiba>
    Reviewed-by: Pavel Machek <pavel@nabladev.com>

--- drivers/net/can/rcar/rcar_canfd.c
+++ drivers/net/can/rcar/rcar_canfd.c
@@ -2199,8 +2199,8 @@ fail_channel:
 fail_mode:
 	rcar_canfd_disable_global_interrupts(gpriv);
 fail_reset:
-	reset_control_assert(gpriv->rstc1);
 	reset_control_assert(gpriv->rstc2);
+	reset_control_assert(gpriv->rstc1);
 fail_clk:
 	clk_disable_unprepare(gpriv->clkp);
 fail_dev:
@@ -2222,9 +2222,9 @@ static void rcar_canfd_remove(struct platform_device *pdev)
 
 	/* Enter global sleep mode */
 	rcar_canfd_set_bit(gpriv->base, RCANFD_GCTR, RCANFD_GCTR_GSLPR);
-	reset_control_assert(gpriv->rstc1);
-	reset_control_assert(gpriv->rstc2);
 	clk_disable_unprepare(gpriv->clkp);
+	reset_control_assert(gpriv->rstc2);
+	reset_control_assert(gpriv->rstc1);
 }
 
 static int __maybe_unused rcar_canfd_suspend(struct device *dev)
